79378b860f79d4cf1293831f93458a330d633b02
satankaos <71780447+satankaos@users.noreply.github.com>
2021-04-13 18:10:06 +0200

    Add files via upload

--- proyecto.sh
+++ proyecto.sh
@@ -10,23 +10,33 @@ read -p "ahora introduce el alias  del server www.ejemplo.com: " aliase
 echo el nombre de servidor es $aliase
 read -p "ahora introduce la ip de el host: " ip
 echo el nombre de servidor es $ip
+opcion=-1
+while [ $opcion -ne 0 ]
+do
+clear
 cat TITULO.TXT
+
 echo "1. Crear Carpetas Necesarias "
 echo "2.Actualizar Repositorios "
 echo "3.Actualizar Paquetes "
 echo "4.Instalar Apache server"
 echo "5.Configurar Arapache "
 echo "6.Mostrar Host "
-echo "2.Actualizar Repositorios "
-read -p "introduzca la opcion por orden si no entiende  " opcion
+echo "0.Actualizar Repositorios "
+read -p "introduzca la opcion por orden si no entiende:  " opcion
 case $opcion in
-1)  mkdir -p /var/www/$server/public_html
+0);;
+1) chmod -R 755 /var/www
 
-chmod -R 755 /var/www
+ mkdir -p /var/www/$server/public_html
 
-cp index.html /var/www/$server/public_html ;;
-2)   ;
-3)   ;
-4)  ;
-5)  ;
-6)  ;
+cp index.html /var/www/$server/public_html;;
+
+2)   ;;
+3)   ;;
+4)  ;;
+5)  ;;
+6)  ;;
+
+esac
+done
