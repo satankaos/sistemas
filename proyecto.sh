f0ccfe5209fd0309ba1e882ebf4119702374d17e
satankaos <71780447+satankaos@users.noreply.github.com>
2021-04-14 08:51:57 +0200

    Add files via upload

--- proyecto.sh
+++ proyecto.sh
@@ -22,8 +22,8 @@ echo "2. Actualizar Paquetes "
 echo "3. Actualizar Repositorios  "
 echo "4.Instalar Apache server"
 echo "5.Configurar Arapache "
-echo "6.Mostrar Host "
-echo "0.Actualizar Repositorios "
+echo "6.Mostrar resulltado "
+echo "0.Salir "
 read -p "introduzca la opcion por orden si no entiende:  " opcion
 case $opcion in
 0);;
@@ -34,11 +34,11 @@ echo "Asignando permisos a la carpeta anterior"
 chmod -R 777 /var/www
  mkdir -p /var/www/$server/public_html
 chmod -R 777 /var/www
-cp index.html /var/www/$server/public_html;;
+cp -RT copy /var/www/$server/public_html;;
 
 2) sudo apt-get update ;;
 3)  sudo apt-get upgrade ;;
-4)  ;;
+4)sudo apt install apache2 ;;
 5) echo -e "<VirtualHost *:80>\v" >> /etc/apache2/sites-available/$server.conf
 echo -e "\tServerAdmin $admin" >>/etc/apache2/sites-available/$server.conf 
 echo -e "\tServername $server" >>/etc/apache2/sites-available/$server.conf 
@@ -50,7 +50,8 @@ echo -e "</VirtualHost>" >> /etc/apache2/sites-available/$server.conf
 a2ensite $server.conf
 a2dissite 000-default.conf
 systemctl restart apache2  ;;
-6)  ;;
+6)
+echo " escribe $server en el buscador ";;
 
 esac
 done
