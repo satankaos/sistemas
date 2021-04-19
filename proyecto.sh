c684e51ae1e03133b8b76fc6b0d67424152a3509
satankaos <71780447+satankaos@users.noreply.github.com>
2021-04-19 13:12:34 +0200

    Update proyecto.sh

--- proyecto.sh
+++ proyecto.sh
@@ -39,7 +39,8 @@ cp -RT copy /var/www/$server/public_html;;
 2) sudo apt-get update ;;
 3)  sudo apt-get upgrade ;;
 4)sudo apt install apache2 ;;
-5) echo -e "<VirtualHost *:80>\v" >> /etc/apache2/sites-available/$server.conf
+5)  sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$server.conf
+echo -e "<VirtualHost *:80>\v" >> /etc/apache2/sites-available/$server.conf
 echo -e "\tServerAdmin $admin" >>/etc/apache2/sites-available/$server.conf 
 echo -e "\tServername $server" >>/etc/apache2/sites-available/$server.conf 
 echo -e "\tServerAdmin $aliase" >>/etc/apache2/sites-available/$server.conf 
