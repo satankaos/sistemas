a521b736125c06f0823657065cc7f8e7f0013042
satankaos <71780447+satankaos@users.noreply.github.com>
2021-04-20 16:48:58 +0200

    Update proyecto.sh

--- proyecto.sh
+++ proyecto.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+opcion=-1
+while [ $opcion -ne 0 ]
+do
 read -p "introduce tu nombre: " nombre
 echo hola $nombre comencemos 
 read -p "ahora introduce el gmail del admin: " admin
@@ -10,11 +12,10 @@ echo creando nombre de servidor
  aliase=www.$server
 echo el nombre de servidor es $aliase
 read -p "ahora introduce la ip de el host: " ip
-echo el nombre de servidor es $ip
-opcion=-1
-while [ $opcion -ne 0 ]
-do
+echo la ip  es $ip
+
 clear
+
 cat TITULO.TXT
 
 echo "1. Instalar Apache server "
@@ -22,7 +23,7 @@ echo "2. Actualizar Paquetes "
 echo "3. Actualizar Repositorios  "
 echo "4. Crear Carpetas Necesarias"
 echo "5.Configurar Arapache "
-echo "6.Mostrar resulltado "
+echo "6.levantar server "
 echo "0.Salir "
 read -p "introduzca la opcion por orden si no entiende:  " opcion
 case $opcion in
@@ -32,33 +33,43 @@ sudo apt install apache2
 ;;
 
 2) sudo apt-get update ;;
+
 3)  sudo apt-get upgrade ;;
+
 4) chmod -R 777 /var/www
-mkdir -p /var/www/$server/public_html
-chmod -R 777 /var/www
-cp -RT copy /var/www/$server/public_html ;;
 
-5) cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/pag.conf
-chmod -R 777 /etc/apache2/sites-available
- 
-echo -e "<VirtualHost *:80>">>/etc/apache2/sites-available/pag.conf
+mkdir -p /var/www/$server
 
-echo -e "\tServerAdmin $admin">>/etc/apache2/sites-available/pag.conf
- 
-echo -e "\tServername $server">>/etc/apache2/sites-available/pag.conf
- 
-echo -e "\tServerAdmin $aliase">>/etc/apache2/sites-available/pag.conf 
+cp -RT copy /var/www/$server ;;
 
-echo -e "\tDocumentRoot /var/www/server">>/etc/apache2/sites-available/pag.conf
- 
-echo -e "\tErrorLog ${APACHE_LOG_DIR}/error.log">>/etc/apache2/sites-available/pag.conf
- 
-echo -e "\tCustomLog ${APACHE_LOG_DIR}/access.log combined">>/etc/apache2/sites-available/pag.conf
- 
-echo -e "</VirtualHost>">>/etc/apache2/sites-available/pag.conf;;
+5) sudo chmod -R 777 /etc/ 
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/pag.conf
+echo > /etc/apache2/sites-available/prueva.conf
 
-6)
-echo " escribe $server o $ip en el buscador " ;;
+echo -e "<VirtualHost *:80>\v" >> /etc/apache2/sites-available/pag.conf
+
+echo -e "\tServerAdmin $admin" >>/etc/apache2/sites-available/pag.conf
+ 
+echo -e "\tServername $server" >>/etc/apache2/sites-available/pag.conf
+ 
+echo -e "\tServerAdmin $aliase" >>/etc/apache2/sites-available/pag.conf 
+
+echo -e "\tDocumentRoot /var/www/$server" >>/etc/apache2/sites-available/pag.conf
+ 
+echo -e "\tErrorLog ${APACHE_LOG_DIR}/error.log" >>/etc/apache2/sites-available/pag.conf
+ 
+echo -e "\tCustomLog ${APACHE_LOG_DIR}/access.log combined\v" >>/etc/apache2/sites-available/pag.conf
+ 
+echo -e "</VirtualHost> \v" >> /etc/apache2/sites-available/pag.conf
+echo -e "# vim: syntax=apache ts=4 sw=4 sts=4 sr noet" >> /etc/apache2/sites-available/pag.conf;;
+
+6) sudo a2ensite pag.conf
+
+sudo a2dissite 000-default.conf
+
+sudo systemctl restart apache2 
+
+echo -e "$ip \t \t $server" >> /etc/hosts;;
 esac
 done
 
